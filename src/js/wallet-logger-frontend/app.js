25772ec3f32310b2798171d27fe46c3817890f9c
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-04 13:52:36 +0200

    Register routes

--- src/js/wallet-logger-frontend/app.js
+++ src/js/wallet-logger-frontend/app.js
@@ -1,9 +1,43 @@
 var wlfApp = angular.module('walletLogger', [
+  'ngRoute',
+
   'wallets',
   'accounts',
   'transactions'
 ]);
 
-wlfApp.constant('mainSettings', {
+wlfApp.constant('appSettings', {
   base_url: 'http://localhost:9999'
 });
+
+wlfApp.config(['$locationProvider', '$routeProvider',
+  function config($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+
+    $routeProvider
+      .when('/', {
+        redirectTo: '/wallets'
+      })
+      .when('/wallets', {
+        template: '<wallets-list></wallets-list>'
+      })
+      .when('/wallets/:walletId', {
+        template: '<wallet-detail></wallet-detail>' + '<accounts-list-for-a-wallet></accounts-list-for-a-wallet>'
+      })
+      .when('/wallets/:walletId/accounts', {
+        template: '<accounts-list></accounts-list>'
+      })
+      .when('/wallets/:walletId/accounts/:accountId', {
+        template: '<account-detail></account-detail>' + '<transactions-list-for-an-account></transactions-list-for-an-account>'
+      })
+      .when('/wallets/:walletId/accounts/:accountId/transactions', {
+        template: '<transactions-list></transactions-list>'
+      })
+      .when('/wallets/:walletId/accounts/:accountId/transactions/:transactionId', {
+        template: '<transaction-detail></transaction-detail>'
+      })
+      .otherwise('/error', {
+        template: '<div class="">Sorry, your requested content does not exists</div>'
+      });
+  }
+]);
