97375b255858f5b7ab5ae94f664d75359a5e5320
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-05 23:23:45 +0200

    Add filter for euro currency

--- src/js/wallet-logger-frontend/app.js
+++ src/js/wallet-logger-frontend/app.js
@@ -48,3 +48,11 @@ wlfApp.config(['$locationProvider', '$routeProvider',
       .otherwise('/error');
   }
 ]);
+
+wlfApp.filter('euroFormat', function () {
+  return function (value) {
+    return value.toFixed(2).replace(/\./, ',').replace(/./g, function(c, i, a) {
+      return i && c !== "," && ((a.length - i) % 3 === 0) ? '.' + c : c;
+    });
+  }
+});
