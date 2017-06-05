b09d28da160a46a13646cfef0204c8f07c4478a4
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-05 23:30:10 +0200

    Format date before and post DB

--- src/js/wallet-logger-frontend/transactions/transactions.controller.js
+++ src/js/wallet-logger-frontend/transactions/transactions.controller.js
@@ -1,4 +1,4 @@
-wlModuleTransactions.controller('TransactionsController', ['$http', '$routeParams', 'appSettings', '$scope', '$location', function ($http, $routeParams, appSettings, $scope, $location) {
+wlModuleTransactions.controller('TransactionsController', ['$http', '$routeParams', 'appSettings', '$scope', '$location', '$filter', function ($http, $routeParams, appSettings, $scope, $location, $filter) {
   var $current_ctrl = this;
   $current_ctrl.current_wallet_id = $routeParams.walletId;
   $current_ctrl.current_account_id = $routeParams.accountId;
@@ -16,12 +16,13 @@ wlModuleTransactions.controller('TransactionsController', ['$http', '$routeParam
     $http.get(appSettings.api_base_url + "/wallets/" + $current_ctrl.current_wallet_id + "/accounts/" + $current_ctrl.current_account_id + "/transactions/" + $routeParams.transactionId)
       .then(function (response) {
         $current_ctrl.selected_transaction = response.data.item;
+        $current_ctrl.selected_transaction.transaction_date = $filter('date')($current_ctrl.Date($current_ctrl.selected_transaction.transaction_date), 'dd/MM/yyyy', '+0200');
       });
   };
 
   $current_ctrl.createItem = function () {
     var new_transaction_data = {
-      transaction_date: $scope.new_transaction_transaction_date,
+      transaction_date: $current_ctrl.dateTime($scope.new_transaction_transaction_date),
       description: $scope.new_transaction_description,
       direction: $scope.new_transaction_direction,
       amount: $scope.new_transaction_amount
@@ -34,7 +35,7 @@ wlModuleTransactions.controller('TransactionsController', ['$http', '$routeParam
 
   $current_ctrl.updateItem = function () {
     var transaction_data = {
-      transaction_date: $current_ctrl.selected_transaction.transaction_date,
+      transaction_date: $current_ctrl.dateTime($current_ctrl.selected_transaction.transaction_date),
       description: $current_ctrl.selected_transaction.description,
       direction: $current_ctrl.selected_transaction.direction,
       amount: $current_ctrl.selected_transaction.amount
@@ -43,6 +44,27 @@ wlModuleTransactions.controller('TransactionsController', ['$http', '$routeParam
       .then(function (response) {
         $location.path("/wallets/" + $current_ctrl.current_wallet_id + "/accounts/" + $current_ctrl.current_account_id + "/transactions/" + response.data.item.id);
       });
+  };
+
+  $current_ctrl.Date = function(date) {
+    return new Date(date);
+  };
+
+  $current_ctrl.itDate = function (date) {
+    console.log(date);
+    var it_date = date.split('/');
+    return $current_ctrl.Date($current_ctrl.Date(it_date[1] + '/' + it_date[0] + '/' + it_date[2]));
+  };
+
+  $current_ctrl.dateTime = function (dateObj) {
+    return dateObj.toISOString().replace(/T/,' ').replace(/\.000Z/,'');
+  };
+
+  $scope.dateOptions = {
+    changeYear: true,
+    changeMonth: true,
+    yearRange: '1900:-0',
+    dateFormat: 'dd/mm/yy'
   };
 
   /*
