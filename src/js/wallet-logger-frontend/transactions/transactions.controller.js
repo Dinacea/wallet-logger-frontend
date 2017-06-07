eef5cb9707d45f73fa70ee1d76127ceb259b7208
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-07 15:52:23 +0200

    Fix delete functions

--- src/js/wallet-logger-frontend/transactions/transactions.controller.js
+++ src/js/wallet-logger-frontend/transactions/transactions.controller.js
@@ -66,7 +66,7 @@ wlModuleTransactions.controller('TransactionsController', ['$http', '$routeParam
 
 
   $current_ctrl.deleteItem = function () {
-    $http.delete(appSettings.api_base_url + "/wallets/" + $routeParams.walletId)
+    $http.delete(appSettings.api_base_url + "/wallets/" + $current_ctrl.current_wallet_id + "/accounts/" + $current_ctrl.current_account_id + "/transactions/" + $routeParams.transactionId)
       .then(function () {
         $location.path("/wallets/" + $current_ctrl.current_wallet_id + "/accounts/" + $current_ctrl.current_account_id);
       });
--- src/js/wallet-logger-frontend/wallets/wallets.controller.js
+++ src/js/wallet-logger-frontend/wallets/wallets.controller.js
@@ -40,7 +40,7 @@ wlModuleWallets.controller('WalletsController', ['$http', '$routeParams', 'appSe
   };
 
   $current_ctrl.deleteItem = function () {
-    $http.delete(appSettings.api_base_url + "/wallets/" + $current_ctrl.current_wallet_id + "/accounts/" + $current_ctrl.current_account_id + "/transactions/" + $routeParams.transactionId)
+    $http.delete(appSettings.api_base_url + "/wallets/" + $routeParams.walletId)
       .then(function () {
         $location.path("/wallets");
       });
