38f2ddd748845eeaf5a680993930ae9fcdd73cbc
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-09 15:57:13 +0200

    Add authentication check on wallets controller

--- src/js/wallet-logger-frontend/wallets/wallets.controller.js
+++ src/js/wallet-logger-frontend/wallets/wallets.controller.js
@@ -1,11 +1,31 @@
-wlModuleWallets.controller('WalletsController', ['$http', '$routeParams', 'appSettings', '$scope', '$location', function ($http, $routeParams, appSettings, $scope, $location) {
+wlModuleWallets.controller('WalletsController', ['$http', '$routeParams', 'appSettings', '$scope', '$location', 'localStorageService', function ($http, $routeParams, appSettings, $scope, $location, localStorageService) {
   var $current_ctrl = this;
-
   $current_ctrl.wallets = [];
+  $current_ctrl.message = { msgStr: false, style: false};
+
   $current_ctrl.getItems = function () {
-    $http.get(appSettings.api_base_url + "/wallets")
+    $http({
+        url: appSettings.api_base_url + "/wallets",
+        method: 'GET',
+        headers: {
+          'Authorization': 'bearer ' + localStorageService.get('token')
+        }
+    })
       .then(function (response) {
         $current_ctrl.wallets = response.data.item;
+      }, function (response) {
+        switch (response.status) {
+          case 401:
+            console.log(response);
+            $location.path("/login");
+            break;
+          default:
+            $current_ctrl.message = {
+              msgStr: 'Sorry, there\'s a problem on server, retry later please',
+              style: 'warning'
+            };
+            break;
+        }
       });
   };
 
@@ -14,8 +34,21 @@ wlModuleWallets.controller('WalletsController', ['$http', '$routeParams', 'appSe
     $http.get(appSettings.api_base_url + "/wallets/" + $routeParams.walletId)
       .then(function (response) {
         $current_ctrl.selected_wallet = response.data.item;
-      }, function () {
-        $location.path("/wallets");
+      }, function (response) {
+        switch (response.status) {
+          case 401:
+            $location.path("/login");
+            break;
+          case 404:
+            $location.path("/wallets");
+            break;
+          default:
+            $current_ctrl.message = {
+              msgStr: 'Sorry, there\'s a problem on server, retry later please',
+              style: 'warning'
+            };
+            break;
+        }
       });
   };
 
@@ -26,6 +59,18 @@ wlModuleWallets.controller('WalletsController', ['$http', '$routeParams', 'appSe
     $http.post(appSettings.api_base_url + "/wallets", $.param(new_wallet_data), {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
       .then(function (response) {
         $location.path("/wallets/" + response.data.item.id);
+      }, function (response) {
+        switch (response.status) {
+          case 401:
+            $location.path("/login");
+            break;
+          default:
+            $current_ctrl.message = {
+              msgStr: 'Sorry, there\'s a problem on server, retry later please',
+              style: 'warning'
+            };
+            break;
+        }
       });
   };
 
@@ -36,6 +81,18 @@ wlModuleWallets.controller('WalletsController', ['$http', '$routeParams', 'appSe
     $http.put(appSettings.api_base_url + "/wallets/" + $current_ctrl.selected_wallet.id, $.param(wallet_data), {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
       .then(function (response) {
         $location.path("/wallets/" + response.data.item.id);
+      }, function (response) {
+        switch (response.status) {
+          case 401:
+            $location.path("/login");
+            break;
+          default:
+            $current_ctrl.message = {
+              msgStr: 'Sorry, there\'s a problem on server, retry later please',
+              style: 'warning'
+            };
+            break;
+        }
       });
   };
 
@@ -43,6 +100,18 @@ wlModuleWallets.controller('WalletsController', ['$http', '$routeParams', 'appSe
     $http.delete(appSettings.api_base_url + "/wallets/" + $routeParams.walletId)
       .then(function () {
         $location.path("/wallets");
+      }, function (response) {
+        switch (response.status) {
+          case 401:
+            $location.path("/login");
+            break;
+          default:
+            $current_ctrl.message = {
+              msgStr: 'Sorry, there\'s a problem on server, retry later please',
+              style: 'warning'
+            };
+            break;
+        }
       });
   };
 }]);
