b42ded33f8e9e88e246b46c6f198753a15a4ded3
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-09 15:07:43 +0200

    Disable uglyfication of JS

--- src/public/Gruntfile.js
+++ src/public/Gruntfile.js
@@ -75,5 +75,5 @@ module.exports = function(grunt) {
 
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify', 'sass']);
+  grunt.registerTask('default', ['concat', 'sass']);
 };
